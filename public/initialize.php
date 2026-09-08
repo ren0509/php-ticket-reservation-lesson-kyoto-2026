c76ceb697457f6f7eed57a0b42026715590e7d6c
ren0509 <maru2if@icloud.com>
2026-09-08 17:41:47 +0900

    DB接続二重化の防止

--- public/initialize.php
+++ public/initialize.php
@@ -25,25 +25,30 @@ $twig = new Environment($loader, [
 
 //DB接続取得
     function getDbh(): PDD{
-        // DBハンドルの取得
-    $config = require __DIR__ . '/../config.php';
-    $db_config = $config['db'];
-    $dsn = "mysql:dbname={$db_config['database']};host={$db_config['host']};port={$db_config['port']};charset={$db_config['charset']}";
+    //二重接続を防ぐためにstatic変数を使う
+    static $dbh = null;
 
-    $opt = [
-        // セキュリティ上必須
-        PDO::ATTR_EMULATE_PREPARES => false,  // エミュレート無効
-        Pdo\Mysql::ATTR_MULTI_STATEMENTS => false,  // 複文無効
-        // お好みで
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // データ取得モード
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // エラーが発生した場合、PDOException をスロー
-    ];
-    try {
-        $dbh = new \PDO($dsn, $db_config['user'], $db_config['pass'], $opt);
-    } catch (\PDOException $e) {
-        // XXX 暫定: 本来はlogに出力する & エラーページを出力する
-        echo $e->getMessage();
-        exit;
+    if (null === $dbh) {
+        // DBハンドルの取得
+        $config = require __DIR__ . '/../config.php';
+        $db_config = $config['db'];
+        $dsn = "mysql:dbname={$db_config['database']};host={$db_config['host']};port={$db_config['port']};charset={$db_config['charset']}";
+
+        $opt = [
+            // セキュリティ上必須
+            PDO::ATTR_EMULATE_PREPARES => false,  // エミュレート無効
+            Pdo\Mysql::ATTR_MULTI_STATEMENTS => false,  // 複文無効
+            // お好みで
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // データ取得モード
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // エラーが発生した場合、PDOException をスロー
+        ];
+        try {
+            $dbh = new \PDO($dsn, $db_config['user'], $db_config['pass'], $opt);
+        } catch (\PDOException $e) {
+            // XXX 暫定: 本来はlogに出力する & エラーページを出力する
+            echo $e->getMessage();
+            exit;
+        }
     }
     // var_dump($dbh);
     // exit;
